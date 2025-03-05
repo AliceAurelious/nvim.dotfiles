bc7d440304b5592c940d8dca6fe301e394305226
Alice Wagner <awagner@synyx.de>
2025-03-05 08:46:13 +0100

    late_night_fixes_with_hase

--- lua/plugins/mini-vim.lua
+++ lua/plugins/mini-vim.lua
@@ -17,5 +17,7 @@ return{
 				markdown = true,
 			},
 		}
+		require("mini.icons").setup()
+		MiniIcons.mock_nvim_web_devicons()
 	end,
 }
--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -5,5 +5,6 @@ return{
 			name = "godot",
 			cmd = vim.lsp.rpc.connect("127.0.0.1", "6005"),
 		})
+		require("lspconfig")["lua_ls"].setup({})
 	end
 }
